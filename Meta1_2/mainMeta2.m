5eb1208259384a9489958d8b8e6b0d548cba6e08
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2024-04-08 22:17:02 +0100

    a

--- Meta1_2/mainMeta2.m
+++ Meta1_2/mainMeta2.m
@@ -1,8 +1,18 @@
-audioFeatures = zeros(50, 10, 5);
+Fs = 48000;
 
-% Start by plotting the first sample of each digit (id = 0)
-figure(1);
-getSpectrum(1, 1);
-sgtitle('Signal examples');
-% i + 1 because MATLAB uses 1-based indexing but the audio file names use 0-based indexing
+audioSignals = preProcess(1)
 
+figure;
+for i = 1:10
+    subplot(5,2,i);
+    fftData = fft(audioSignals{i});
+    fftData = fftData(1:floor(length(fftData)/2)); % only take the first half of the data because the second half is just a mirror image
+
+    ampSpectrum = abs(fftData); % get the magnitude of the complex fourier series coefficients
+    ampSpectrum = ampSpectrum/length(fftData); % normalize by the number of samples
+
+    plot(ampSpectrum(1:3000), 'Color', 'red'); % set the color to yellow
+    title(['Amplitude Spectrum of Digit ', num2str(i)]);
+    xlabel('Frequency (Hz)');
+    ylabel('Amplitude');
+end
