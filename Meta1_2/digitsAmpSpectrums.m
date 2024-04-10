c53547c692139bedb1e026c31d2caa3f5f648e71
NunoMBatista <118256333+NunoMBatista@users.noreply.github.com>
2024-04-10 21:21:05 +0100

    meta 2 - 5, 6, 7*0.5

---
+++ Meta1_2/digitsAmpSpectrums.m
@@ -0,0 +1,58 @@
+% Meta 2
+
+function [medianAmpSpectrumMat, Q25AmpSpectrumMat, Q75AmpSpectrumMat, meanAmpSpectrumMat, everyAmpSpectrumMat] = digitsAmpSpectrums(audioSignals, windowType)
+    % Each index of audioSignals contains the audio signals of each one of the digits from that sample
+
+    freqNum = 3000; % number of frequencies to plot
+    medianAmpSpectrumMat = zeros(freqNum, 10); % matrix to store the median amplitude spectrum of each digit
+    Q25AmpSpectrumMat = zeros(freqNum, 10); % matrix to store the 25th percentile amplitude spectrum of each digit
+    Q75AmpSpectrumMat = zeros(freqNum, 10); % matrix to store the 75th percentile amplitude spectrum of each digit
+    meanAmpSpectrumMat = zeros(freqNum, 10); % matrix to store the mean amplitude spectrum of each digit
+
+    everyAmpSpectrumMat = cell(10, 1); % matrix to store the amplitude spectrum matrix of each digit
+
+    figure;
+    for digit = 1:10 % for each digit
+        ampSpectrumMat = zeros(freqNum, 50);
+        for i = 1:50 % for each sample
+
+            if strcmp(windowType, 'hamming')
+                windowedSignal = audioSignals{i}{digit} .* hamming(length(audioSignals{i}{digit})); % apply hamming window to the audio signal
+            elseif strcmp(windowType, 'hanning')
+                windowedSignal = audioSignals{i}{digit} .* hanning(length(audioSignals{i}{digit})); % apply hanning window to the audio signal
+            elseif strcmp(windowType, 'blackman')
+                windowedSignal = audioSignals{i}{digit} .* blackman(length(audioSignals{i}{digit})); % apply blackman window to the audio signal
+            else
+                windowedSignal = audioSignals{i}{digit}; % do not apply any window to the audio signal
+            end
+
+            curDigitFFT = fft(windowedSignal); % get the fourier series coefficients of the current sample of the current digit
+            %curDigitFFT = fft(audioSignals{i}{digit}); % get the fourier series coefficients of the current sample of the current digit
+            
+            curDigitFFT = curDigitFFT(1:floor(length(curDigitFFT)/2)); % only take the first half of the data because the second half is symmetrical
+
+            ampSpectrum = abs(curDigitFFT); % get the magnitude of the complex fourier series coefficients
+            ampSpectrum = ampSpectrum/length(curDigitFFT); % normalize by the number of samples
+        
+            ampSpectrumMat(:, i) = ampSpectrum(1:freqNum); % store the amplitude spectrum of the current sample of the current digit
+        end
+
+        everyAmpSpectrumMat{digit} = ampSpectrumMat;
+
+        medianAmpSpectrumMat(:, digit) = median(ampSpectrumMat, 2); % get the median amplitude spectrum of the digit
+        Q25AmpSpectrumMat(:, digit) = quantile(ampSpectrumMat, 0.25, 2); % get the 25th percentile 
+        Q75AmpSpectrumMat(:, digit) = quantile(ampSpectrumMat, 0.75, 2); % get the 75th percentile 
+        meanAmpSpectrumMat(:, digit) = mean(ampSpectrumMat, 2); % get the mean amplitude spectrum of the digit
+
+        subplot(5, 2, digit);
+        plot(medianAmpSpectrumMat(:, digit), 'Color', 'red');
+        hold on;
+        plot(Q25AmpSpectrumMat(:, digit), 'Color', 'green');
+        plot(Q75AmpSpectrumMat(:, digit), 'Color', 'blue');
+        hold off;
+        title('Median, Q25 and Q75 Amplitude Spectrum of Digit 5, with windowtype ', windowType);
+        xlabel('Frequency (Hz)');
+        ylabel('Amplitude');
+        legend('Median', 'Q25', 'Q75');
+    end
+end
--- Meta1_2/mainMeta2.m
+++ Meta1_2/mainMeta2.m
@@ -1,18 +1,36 @@
-Fs = 48000;
+freqNum = 3000; % number of frequencies to plot
+medianAmpSpectrumMat = zeros(freqNum, 10); % matrix to store the median amplitude spectrum of each digit
+Q25AmpSpectrumMat = zeros(freqNum, 10); % matrix to store the 25th percentile amplitude spectrum of each digit
+Q75AmpSpectrumMat = zeros(freqNum, 10); % matrix to store the 75th percentile amplitude spectrum of each digit
+everyAmpSpectrumMat = cell(10, 1); % matrix to store the amplitude spectrum of each digit
 
-audioSignals = preProcess(1);
-
-figure;
-for i = 1:10
-    subplot(5,2,i);
-    fftData = fft(audioSignals{i});
-    fftData = fftData(1:floor(length(fftData)/2)); % only take the first half of the data because the second half is just a mirror image
-
-    ampSpectrum = abs(fftData); % get the magnitude of the complex fourier series coefficients
-    ampSpectrum = ampSpectrum ./ length(fftData); % normalize by the number of samples
-
-    plot(ampSpectrum(1:3000), 'Color', 'red'); % set the color to yellow
-    title(['Amplitude Spectrum of Digit ', num2str(i)]);
-    xlabel('Frequency (Hz)');
-    ylabel('Amplitude');
+audioSignals = cell(50, 1); % Each one of the 50 samples will contain the audio signals of each one of the digits
+for i = 1:50
+    audioSignals{i} = preProcess(i-1); % get the audio signals of each one of the digits from the sample
 end
+
+meanAmpSpectrumMat = zeros(freqNum, 10); % matrix to store the mean amplitude spectrum of each digit
+[medianAmpSpectrumMat, Q25AmpSpectrumMat, Q75AmpSpectrumMat, meanAmpSpectrumMat, everyAmpSpectrumMat] = digitsAmpSpectrums(audioSignals, 'default');
+
+% digitsAmpSpectrums('hamming');
+% digitsAmpSpectrums('hann');
+% digitsAmpSpectrums('blackman');
+
+spectralFeaturesStrings = {'Spectral Mean'};
+
+spectralFeatures = containers.Map(); % Hashmap containing the spectral features of each digit
+
+% Get spectral mean of each digit
+spectralMean = cell(10, 1);
+curSampleSpectralMean = cell(50, 1);
+for digit = 1:10
+    for sample = 1:50
+        curSampleSpectralMean{sample} = mean(everyAmpSpectrumMat{digit}(:, sample), 1);
+    end
+    spectralMean{digit} = curSampleSpectralMean;
+end
+spectralFeatures('Spectral Mean') = spectralMean;
+
+
+% Make a boxplot of the spectral mean of each digit
+boxplotFeature(spectralFeatures('Spectral Mean'), 'Spectral Mean', 'Boxplot of Spectral Means for Each Digit');
